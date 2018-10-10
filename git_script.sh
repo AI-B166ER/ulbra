9cb28f94a5f759cb29b601537913104a86cca5f3
Ramon <newripper666@gmail.com>
2018-10-09 23:49:01 -0300

    ramon

--- git_script.sh
+++ git_script.sh
@@ -1,1 +1,9 @@
-git add . && git commit -m "commit" && git push origin master
+#!/bin/bash
+
+if [ "$1" == "" ]
+then
+	echo "Modo de usar: ./git_script <texto do commit>"
+else
+	git add . && git commit -m "$1" && git push origin master
+fi
+
